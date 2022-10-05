ebd5d5ba94921f304cc98cb3708a922165b36747
mcworkaholic <wevans302@gmail.com>
2022-10-05 14:30:32 -0500

    added autoDelete() and addUsers() to main.py

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ putty = app.PuTTY
 # resets and forces the expiration of user's password
 # users are defined within "user-reset.xlsx" with a temporary password of "temp_password" defined in the .env 
 # that pre-defined password will have to be used and changed upon the user's next login
-
 def autoReset():
     df = pd.read_excel("C:\\path\\to\\user-reset.xlsx") 
     user_list = df['username'].tolist()
@@ -54,8 +53,27 @@ def autoReset():
         putty.type_keys("sudo" + sp + "passwd" + sp + "-e" + sp + str(i))
         putty.type_keys(e) 
 
+#  **USE WITH CAUTION** auto-deletes users as defined in userdel.xlsx  
+def autoDelete():
+    df = pd.read_excel("C:\\path\\to\\userdel.xlsx") 
+    user_list = df['username'].tolist()
+    putty.type_keys("vim" + sp + "userdel.txt" + e)
+    putty.type_keys("i")
+    for i in user_list:
+        time.sleep(.5)
+        putty.type_keys(i + sp + e)
+    putty.type_keys("{ESC}")
+    putty.type_keys("{VK_SHIFT down}" "ZZ" "{VK_SHIFT up}")
+    putty.type_keys("cat userdel.txt" + e, with_spaces=True)
+    putty.type_keys("for user in `cat userdel.txt`;do userdel $user;done" + e, with_spaces=True)
+    print(user_list)
+
+# bulk creation of users based on useradd.xlsx, to be added
+def addUsers():
+    pass
+
+# Initial login
 def login():
-    # Initial login
     putty.wait('ready')
     putty.type_keys(sudo_password)
     putty.type_keys("{ENTER}")
