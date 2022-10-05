38ca6f3d2aaa84e72125f64cc5f04981823599b4
mcworkaholic <94456069+mcworkaholic@users.noreply.github.com>
2022-10-05 13:27:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 from dotenv import load_dotenv
 
 def login():
-    # make your own .env file, copy the contents from "sample.env", paste into .env, change the values in your new .env file to your actual values
-    # define path to .env file
+    # make your own .env file, copy the contents from "sample.env",
+    # paste into .env,
+    # change the values in your new .env file to your actual values
+    # define path to .env file and change path below in line #11
+
     load_dotenv(dotenv_path="C:\\Path\\to\\.env")
 
+    # Setting variables defined in .env
     ip_address = os.getenv("IP_ADDRESS")
     sudo_password = os.getenv("SUDO_PASSWORD")
     user_name = os.getenv("USER_NAME")
